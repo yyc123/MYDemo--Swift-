c2a3e8769dfabc7897fe56e109eb05451c221abd
yyc <645746043@qq.com>
2017-06-24 16:36:11 +0800

    创建工程

---
+++ MYDemo/MYDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MYDemo
+//
+//  Created by 德基 on 2017/6/24.
+//  Copyright © 2017年 yyc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
